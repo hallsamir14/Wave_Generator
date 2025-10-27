955ff4e10a4b44d35cea5e8c5c77373514ae4082
Samir Hall <hallsamir14@gmail.com>
2025-10-26 21:43:45 -0400

    Implement logic to generate unique filename for each generated sample file.

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 	"wave_generator/generator"
 	"wave_generator/writer"
 )
@@ -15,14 +16,21 @@ func main() {
 	const frequency = 220.0
 	const amplitude = 0.3
 	const durationSec = 4
+	const waveType = "Triangle"
 
 	// 16-bit PCM (2 bytes per sample)
 	numSamples := uint(sampleRate * durationSec)
 	data := make([]byte, numSamples*2)
-	data = generator.GenerateWave(numSamples, sampleRate, frequency, amplitude, data)
+	data, generatorErr := generator.GenerateWave(waveType, numSamples, sampleRate, frequency, amplitude, data)
+
+	if generatorErr != nil {
+		fmt.Println("Error generating wave")
+	}
 
 	dir := "tones"
-	filename := "tones.wav"
+	timestamp := time.Now().Format("20060102150405")
+	extension := ".wav"
+	filename := fmt.Sprintf("%v_%v.%v", waveType, timestamp, extension)
 	filePath := filepath.Join(dir, filename)
 
 	err := os.MkdirAll(filepath.Dir(filePath), 0755)
